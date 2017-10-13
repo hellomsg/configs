e5ec3995ace216df48924a1708158e3317e506cb
Wang Binbin <hellomsg_noreply@163.com>
2017-10-13 10:46:33 +0800

    Create config.groovy

---
+++ gradle/config.groovy
@@ -0,0 +1,25 @@
+environments {
+    prod {
+        redis {
+            host = 'localhost'
+            port = 6379
+            username = ''
+            password = ''
+        }
+        jdbc {
+            
+        }
+    }
+
+    dev {
+        redis {
+            host = 'localhost'
+            port = 6379
+            username = ''
+            password = ''
+        }
+        jdbc {
+            
+        }
+    }
+}
